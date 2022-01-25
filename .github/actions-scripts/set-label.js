76b259db0c38b2ebe32f3ed83baacb622fe7f7f4
Peter Bengtsson <mail@peterbe.com>
2022-01-25 09:25:40 -0500

    actions script

---
+++ .github/actions-scripts/set-label.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import { context, getOctokit } from "@actions/github";
+import { setOutput } from "@actions/core";
+
+const octokit = getOctokit();
+
+main();
+
+async function labelPullRequest(config) {
+  const { owner, repo } = context;
+
+  console.log(context);
+  //   await octokit.rest.issues.update({
+  //     owner,
+  //     repo,
+  //     issue_number: config.issue_number,
+  //     labels: config.labels,
+  //   });
+}
+
+async function main() {
+  const label = "my-label";
+  await labelPullRequest([label]);
+  setOutput("myLabel", label);
+}
