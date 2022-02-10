63224579ca03ebd6f195ee16d5fc8f4555b37697
Peter Bengtsson <mail@peterbe.com>
2022-02-09 21:09:04 -0500

    actions script (#2)
    using actions script

---
+++ .github/actions-scripts/get-labels.mjs
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import { context, getOctokit } from "@actions/github";
+import { setOutput } from "@actions/core";
+
+console.assert(process.env.GITHUB_TOKEN, "GITHUB_TOKEN not present");
+
+const octokit = getOctokit(process.env.GITHUB_TOKEN);
+
+main();
+
+async function getCurrentPRLabels() {
+  const {
+    repo: { owner, repo },
+    payload: { number },
+  } = context;
+  console.assert(number, "number not present");
+  const { data: currentLabels } = await octokit.rest.issues.listLabelsOnIssue({
+    owner,
+    repo,
+    issue_number: number,
+  });
+  console.log({ currentLabels });
+  return currentLabels.map((label) => label.name).join(", ");
+}
+
+async function main() {
+  const labels = await getCurrentPRLabels();
+  setOutput("currentLabels", labels);
+}
