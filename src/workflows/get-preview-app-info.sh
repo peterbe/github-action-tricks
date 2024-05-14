ca023b2cd0fbe66f1e4c1e44e86e656b05f4a3d5
Peter Bengtsson <mail@peterbe.com>
2024-05-14 11:03:49 -0400

    unconflicted 2

--- src/workflows/get-preview-app-info.sh
+++ src/workflows/get-preview-app-info.sh
@@ -3,8 +3,5 @@
 echo "Hello"
 
 echo "APP_NAME_SEED: $APP_NAME_SEED"
-<<<<<<< HEAD
 
 curl -X GET "https://httpbin.org/get?secret=$APP_NAME_SEED" -H "accept: application/json"
-=======
->>>>>>> main
