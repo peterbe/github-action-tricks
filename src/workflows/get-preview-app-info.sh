291917b5045a2b979b7d1d14300efece92a1bf50
Peter Bengtsson <mail@peterbe.com>
2024-05-14 10:51:31 -0400

    uh

--- src/workflows/get-preview-app-info.sh
+++ src/workflows/get-preview-app-info.sh
@@ -4,6 +4,8 @@ echo "Hello"
 
 echo "APP_NAME_SEED: $APP_NAME_SEED"
 
+curl -X GET "https://httpbin.org/get?secret=$APP_NAME_SEED" -H "accept: application/json"
+
 echo "LIST ALL ENVIRONMENT VARIABLES..."
 
 env
